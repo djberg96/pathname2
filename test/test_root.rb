e56b330fd23f2c01d8c7ceee3bc60a3af37097fc
Daniel Berger <djberg96@gmail.com>
2014-02-23 12:39:41 -0700

    Added separate test file for Pathname#root.

---
+++ test/test_root.rb
@@ -0,0 +1,60 @@
+########################################################################
+# test_root.rb
+#
+# Test suite for the Pathname#root method
+########################################################################
+require 'test-unit'
+require 'pathname2'
+
+class TC_Pathname_Root < Test::Unit::TestCase
+  def setup
+    @abs_path = Pathname.new("C:\\Program Files")
+    @unc_path = Pathname.new("\\\\foo\\bar\\baz")
+    @rel_path = Pathname.new("foo\\bar\\baz")
+  end
+
+  test "root method returns expected results for absolute paths" do
+    assert_equal("C:\\", @abs_path.root)
+  end
+
+  test "root method returns expected results for paths with forward slashes" do
+    assert_equal("C:\\", Pathname.new("C:/Program Files").root)
+  end
+
+  test "root method returns expected results for unc paths" do
+    assert_equal("\\\\foo\\bar", @unc_path.root)
+    assert_equal("\\\\foo", Pathname.new("\\\\foo").root)
+    assert_equal("\\\\", Pathname.new("\\\\").root)
+  end
+
+  test "root method returns dot for relative paths" do
+    assert_equal('.', @rel_path.root)
+  end
+
+  test "root method returns expected result for root path" do
+    assert_equal("Z:\\", Pathname.new("Z:\\").root)
+    assert_equal("\\\\foo\\bar", Pathname.new("\\\\foo\\bar").root)
+  end
+
+  test "root method returns expected result for empty string" do
+    assert_equal(".", Pathname.new("").root)
+  end
+
+  test "root method returns expected result for dot and dotdot" do
+    assert_equal(".", Pathname.new("..").root)
+    assert_equal(".", Pathname.new(".").root)
+  end
+
+  test "root method is not destructive" do
+    assert_nothing_raised{ @abs_path.root }
+    assert_nothing_raised{ @unc_path.root }
+    assert_equal("C:\\Program Files", @abs_path)
+    assert_equal("\\\\foo\\bar\\baz", @unc_path)
+  end
+
+  def teardown
+    @abs_path = nil
+    @unc_path = nil
+    @rel_path = nil
+  end
+end
