5efddc315b24b237ed2b89928819802eb248ac96
Daniel Berger <djberg96@gmail.com>
2014-03-17 15:26:07 -0400

    Added a separate file for the custom each method.

---
+++ test/windows/test_each.rb
@@ -0,0 +1,28 @@
+########################################################################
+# test_each.rb
+#
+# Test suite for the Pathname#each method.
+########################################################################
+require 'pathname2'
+require 'test-unit'
+
+class TC_Pathname_Each < Test::Unit::TestCase
+  def setup
+    @path = Pathname.new("C:/Users/foo/bar")
+  end
+
+  test "each basic functionality" do
+    assert_respond_to(@path, :each)
+    assert_nothing_raised{ @path.each{} }
+  end
+
+  test "each returns the expected results" do
+    arr = []
+    @path.each{ |e| arr << e }
+    assert_equal(['C:', 'Users', 'foo', 'bar'], arr)
+  end
+
+  def teardown
+    @path = nil
+  end
+end
