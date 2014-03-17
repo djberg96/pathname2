6986a1f7a2a50f3112a1552c0213de0591760069
Daniel Berger <djberg96@gmail.com>
2014-03-17 15:26:22 -0400

    Added a separate test file for just the version.

---
+++ test/test_version.rb
@@ -0,0 +1,13 @@
+########################################################################
+# test_version.rb
+#
+# Universal test file that tests for the proper version number.
+########################################################################
+require 'pathname2'
+require 'test-unit'
+
+class TC_Pathname_Version < Test::Unit::TestCase
+  test "version is set to expected value" do
+    assert_equal('1.7.0', Pathname::VERSION)
+  end
+end
