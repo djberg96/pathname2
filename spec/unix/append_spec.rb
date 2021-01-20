dfe87310284ed81b8e6d43489f9e44a286d99f1b
Daniel Berger <djberg96@gmail.com>
2021-01-20 08:34:19 -0500

    Finish up append specs for now.

--- spec/unix/append_spec.rb
+++ spec/unix/append_spec.rb
@@ -7,21 +7,23 @@ require 'spec_helper'
 require 'pathname2'
 
 RSpec.describe Pathname, :unix => true, :append => true do
-  before do
-    @abs_path = Pathname.new('/usr/local/bin')
-    @rel_path = Pathname.new('usr/local/bin')
-  end
+  let(:abs_path){ Pathname.new('/usr/local/bin') }
+  let(:rel_path){ Pathname.new('usr/local/bin') }
 
   example "pathname_append basic functionality" do
-    expect(@abs_path).to respond_to(:+)
+    expect(abs_path).to respond_to(:+)
   end
 
   example "appending a relative path to an absolute path works as expected" do
     expect(described_class.new("/foo") + "bar").to eq("/foo/bar")
   end
 
-  example "appending an absolute path to a relative path works as expected" do
+  example "appending an absolute path to a relative path returns that absolute path" do
     expect(described_class.new("foo") + "/bar").to eq("/bar")
+  end
+
+  example "appending an absolute path to an absolute path returns that absolute path" do
+    expect(described_class.new("/foo") + "/bar").to eq("/bar")
   end
 
   example "appending a relative path to a relative path works as expected" do
@@ -61,30 +63,9 @@ RSpec.describe Pathname, :unix => true, :append => true do
     expect(described_class.instance_method(:+)).to eq(described_class.instance_method(:/))
   end
 
-=begin
-  example "appending a string to a UNC path works as expected" do
-    expect_pathname_plus("\\\\foo\\bar", "\\\\foo", "bar")
-    expect_pathname_plus("\\\\foo", "\\\\", "foo")
-    expect_pathname_plus("\\\\", "\\\\", "")
-    expect_pathname_plus("\\\\foo\\baz", "\\\\foo\\bar", "\\..\\baz")
-    expect_pathname_plus("\\\\", "\\\\", "..\\..\\..\\..")
+  example "neither the receiver nor the argument to the constructor are modified" do
+    expect{ abs_path + rel_path }.not_to raise_error
+    expect(abs_path).to eq('/usr/local/bin')
+    expect(rel_path).to eq('usr/local/bin')
   end
-
-  example "appending a plain string to a path works as expected" do
-    expect{ @abs_path + "bar" }.not_to raise_error
-    expect( @abs_path + 'baz').to eq('C:\foo\bar\baz')
-    expect( @abs_path).to eq('C:\foo\bar')
-  end
-
-  example "appending an absolute path results in that absolute path" do
-    expect_pathname_plus('C:\foo\bar', @rel_path, @abs_path)
-  end
-
-  example "neither receiver nor argument are modified" do
-    expect{ @abs_path + @rel_path }.not_to raise_error
-    expect( @abs_path + @rel_path).to eq('C:\foo\bar\foo\bar\baz')
-    expect( @abs_path).to eq('C:\foo\bar')
-    expect( @rel_path).to eq('foo\bar\baz')
-  end
-=end
 end
