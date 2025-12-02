3df99e429c3879a169e134c957508e0e846b0c40
Daniel Berger <78529+djberg96@users.noreply.github.com>
2025-12-01 21:15:10 -0500

    Add specs for relative paths.

--- spec/aref_spec.rb
+++ spec/aref_spec.rb
@@ -1,5 +1,5 @@
 ########################################################################
-# append_spec.rb
+# aref_spec.rb
 #
 # Specs for the Pathname2#append method.
 ########################################################################
@@ -7,28 +7,48 @@ require 'spec_helper'
 require 'pathname2'
 
 RSpec.describe Pathname2, :unix => true, :aref => true do
-  let(:path){ described_class.new('/usr/local/bin') }
+  let(:abs_path){ described_class.new('/usr/local/bin') }
+  let(:rel_path){ described_class.new('usr/local/bin') }
 
-  example "[] with index works as expected" do
-    expect(path[0]).to eq("/usr")
-    expect(path[1]).to eq("local")
-    expect(path[-1]).to eq("bin")
+  example "[] with index works as expected for absolute path" do
+    expect(abs_path[0]).to eq("/usr")
+    expect(abs_path[1]).to eq("local")
+    expect(abs_path[-1]).to eq("bin")
+  end
+
+  example "[] with index works as expected for relative path" do
+    expect(rel_path[0]).to eq("usr")
+    expect(rel_path[1]).to eq("local")
+    expect(rel_path[-1]).to eq("bin")
   end
 
   example "[] with index out of range returns nil" do
-    expect(path[10]).to be_nil
+    expect(abs_path[10]).to be_nil
   end
 
-  example "[] with range argument works as expected" do
-    expect(path[0..1]).to eq("/usr/local")
-    expect(path[0..2]).to eq("/usr/local/bin")
-    expect(path[1..2]).to eq("local/bin")
-    expect(path[0..-1]).to eq(path)
+  example "[] with range argument works as expected for absolute path" do
+    expect(abs_path[0..1]).to eq("/usr/local")
+    expect(abs_path[0..2]).to eq("/usr/local/bin")
+    expect(abs_path[1..2]).to eq("local/bin")
+    expect(abs_path[0..-1]).to eq(abs_path)
   end
 
-  example "[] with index and length works as expected" do
-    expect(path[0,1]).to eq("/usr")
-    expect(path[0,2]).to eq("/usr/local")
-    expect(path[1,2]).to eq("local/bin")
+  example "[] with range argument works as expected for relative path" do
+    expect(rel_path[0..1]).to eq("usr/local")
+    expect(rel_path[0..2]).to eq("usr/local/bin")
+    expect(rel_path[1..2]).to eq("local/bin")
+    expect(rel_path[0..-1]).to eq(rel_path)
+  end
+
+  example "[] with index and length works as expected for absolute path" do
+    expect(abs_path[0,1]).to eq("/usr")
+    expect(abs_path[0,2]).to eq("/usr/local")
+    expect(abs_path[1,2]).to eq("local/bin")
+  end
+
+  example "[] with index and length works as expected for relative path" do
+    expect(rel_path[0,1]).to eq("usr")
+    expect(rel_path[0,2]).to eq("usr/local")
+    expect(rel_path[1,2]).to eq("local/bin")
   end
 end
